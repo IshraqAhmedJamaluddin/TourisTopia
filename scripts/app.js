34f36db1285795d7208aad173c6e1a6dd4090cf9
Ishraq Ahmed <ishraqahmedjamaluddin@gmail.com>
2020-05-20 01:21:05 +0200

    searching bar javascript

--- scripts/app.js
+++ scripts/app.js
@@ -42,11 +42,39 @@ function goToCity() {
         return;
     }
     window.location.href = "city.html";
+}
+
+window.onload = () => {
     var xhr = new XMLHttpRequest;
     xhr.onload = () => {
-        document.title=name;
+        var body = XMLHttpRequest.response;
+        //n7mel kol l options mn l db
     }
-    xhr.open("POST","/server.js");
+    xhr.open("GET","/server.js");
     xhr.responseType = JSON;
-    xhr.send(dest);
-};
+    xhr.send();
+}
+
+// Find all inputs on the DOM which are bound to a datalist via their list attribute.
+var inputs = document.querySelectorAll('input[list]');
+for (var i = 0; i < inputs.length; i++) {
+  // When the value of the input changes...
+  inputs[i].addEventListener('change', function() {
+    var optionFound = false,
+      datalist = this.list;
+    // Determine whether an option exists with the current value of the input.
+    for (var j = 0; j < datalist.options.length; j++) {
+        if (this.value == datalist.options[j].value) {
+            optionFound = true;
+            break;
+        }
+    }
+    // use the setCustomValidity function of the Validation API
+    // to provide an user feedback if the value does not exist in the datalist
+    if (optionFound) {
+      this.setCustomValidity('');
+    } else {
+      this.setCustomValidity('Please choose from the available destinations ^_^');
+    }
+  });
+}
