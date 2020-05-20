9b8412019b302958f3a91471f36538ba25a988e6
Ishraq Ahmed <ishraqahmedjamaluddin@gmail.com>
2020-05-20 03:14:18 +0200

    updating paragraphs and headings of dest.html

--- scripts/app2.js
+++ scripts/app2.js
@@ -1,4 +1,28 @@
 window.onload = () => {
     var destination = localStorage.getItem("destName");
-    console.log(destination);
+    document.title = destination;
+    
+    //tb3an hnb3at l title lli fo2 daf POST
+    var xhr = new XMLHttpRequest;
+    xhr.onload = () => {
+        
+        var textnode = document.createTextNode(xhr.response.city);
+        var city = document.createElement("span");
+        city.appendChild(textnode);
+        var textnode = document.createTextNode(xhr.response.place);
+        document.getElementById("h1").appendChild(city);
+        document.getElementById("h1").appendChild(textnode);
+
+        var textnode = document.createTextNode(xhr.response.p1);
+        document.getElementById("p1").appendChild(textnode);
+
+        var textnode = document.createTextNode(xhr.response.heading);
+        document.getElementById("heading").appendChild(textnode);
+        
+        var textnode = document.createTextNode(xhr.response.p2);
+        document.getElementById("p2").appendChild(textnode);
+    }
+    xhr.open("GET","/test2.json");
+    xhr.responseType = "json";
+    xhr.send();
 }
